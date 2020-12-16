04a018c49b5293fbe9b7bc0160e4cd9ee28140b5
Andorelh <sylvain.franco@outlook.fr>
2020-12-16 19:01:14 +0100

    ajout RenderText

--- src/mainTp4.cpp
+++ src/mainTp4.cpp
@@ -87,6 +87,51 @@ float RandomFloat(float a, float b) {
 	return a + r;
 }
 
+void renderText(float x, float y, const char* text, void* font)
+{
+	glDisable(GL_DEPTH_TEST);
+
+	// Temporarily set up the view in orthographic projection.
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	glOrtho(0.0, (double)1280, 0.0, (double)720, -1.0, 1.0);
+
+	// Move to modelview mode.
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+
+	// Ensure we have a font
+	if (font == NULL) {
+		font = GLUT_BITMAP_HELVETICA_10;
+	}
+
+	// Loop through characters displaying them.
+	size_t len = strlen(text);
+
+	glRasterPos2f(x, y);
+	for (const char* letter = text; letter < text + len; letter++) {
+
+		// If we meet a newline, then move down by the line-height
+		// TODO: Make the line-height a function of the font
+		if (*letter == '\n') {
+			y -= 12.0f;
+			glRasterPos2f(x, y);
+		}
+		glutBitmapCharacter(font, *letter);
+	}
+
+	// Pop the matrices to return to how we were before.
+	glPopMatrix();
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+
+	glEnable(GL_DEPTH_TEST);
+}
+
+
 void update()
 {
 	temps.update();
